5c45a7e1c29b6bea7b24bf8c2773f236eaddec76
Thomas Czank <thomas.czank@gmail.com>
2021-04-12 17:30:46 +0900

    mudei a ordem dos posts de novo

--- conf.py
+++ conf.py
@@ -232,7 +232,8 @@ THEME_CONFIG = {
 
 POSTS = (
     ("posts/vozes-da-diaspora-entra-na-luta-por-um-brasil-sem-bolsonarismo.rst", "posts", "post.tmpl"),
-    ("posts/{o-brasil-e-o-mundo-em-2021.rst, estragos-feitos-por-bolsonaro-sao-imensuraveis.rst}", "posts", "post.tmpl"),
+    ("posts/o-brasil-e-o-mundo-em-2021.rst", "posts", "post.tmpl"),
+    ("posts/estragos-feitos-por-bolsonaro-sao-imensuraveis.rst", "posts", "post.mpl"),
     ("posts/*.md", "posts", "post.tmpl"),
     ("posts/*.txt", "posts", "post.tmpl"),
     ("posts/*.html", "posts", "post.tmpl"),
