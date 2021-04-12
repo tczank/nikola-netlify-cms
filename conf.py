465ae49e9a59356054981f48b0e4feefb9d88456
Thomas Czank <thomas.czank@gmail.com>
2021-04-12 16:00:47 +0900

    identacao errada no conf.py

--- conf.py
+++ conf.py
@@ -276,7 +276,7 @@ TIMEZONE = "UTC"
 # 2 = using a string like “2 days ago”
 #
 # Your theme must support it, Bootstrap already does.
- DATE_FANCINESS = 2
+DATE_FANCINESS = 2
 
 # Customize the locale/region used for a language.
 # For example, to use British instead of US English: LOCALES = {'en': 'en_GB'}
