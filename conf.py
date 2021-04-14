80a857ef92d7eccace7accc9b72526e5222f7f19
Thomas Czank <thomas.czank@gmail.com>
2021-04-15 03:48:49 +0900

    far raised fist nao funciona, de volta ao f de facebook

--- conf.py
+++ conf.py
@@ -153,9 +153,7 @@ NAVIGATION_LINKS = {
         ('/categories/index.html', 'Tags', 'fa fa-tags'),
         ('/rss.xml', 'RSS', 'fa fa-rss'),
         ('https://www.facebook.com/forabozonihon', 'Fora Bolsonaro Japão', 'fas fa-bomb'),
-        #('https://www.facebook.com/UNIDXSporUmBrasilmelhor', 'UNIDXS por um Brasil Melhor', 'fab fa-facebook f'),
-        ('https://www.facebook.com/UNIDXSporUmBrasilmelhor', 'UNIDXS por um Brasil Melhor', 'far fa-raised-fist'),
-
+        ('https://www.facebook.com/UNIDXSporUmBrasilmelhor', 'UNIDXS por um Brasil Melhor', 'fab fa-facebook f'),
         ('https://www.facebook.com/nucleo.pt.boston.eua', 'Núcleo PT Boston', 'fab fa-star'),
 #        ('https://getnikola.com', 'Get Nikola', 'fa fa-user'),
 #        ('https://twitter.com/getnikola', 'My Twitter', 'fab fa-twitter'),
