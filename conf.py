9f5d927fc2c65ded7597c783722834b0b2c2a181
Thomas Czank <thomas.czank@gmail.com>
2021-05-03 02:29:18 +0900

    tentando logo

--- conf.py
+++ conf.py
@@ -355,6 +355,8 @@ METADATA_FORMAT = "YAML"
 # Final output is <img src="LOGO_URL" id="logo" alt="BLOG_TITLE">.
 # The URL may be relative to the site root.
 LOGO_URL = 'images/vozesdadiaspora_2.jpg'
+id = "logo"
+alt = "logo"
 
 # If you want to hide the title of your website (for example, if your logo
 # already contains the text), set this to False.
