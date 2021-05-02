f6468e9b6ecb9dee4ae8b5b7c5947cf342a9366b
Thomas Czank <thomas.czank@gmail.com>
2021-05-03 02:32:19 +0900

    tentando logo de novo

--- conf.py
+++ conf.py
@@ -354,9 +354,7 @@ METADATA_FORMAT = "YAML"
 # Nikola supports logo display.  If you have one, you can put the URL here.
 # Final output is <img src="LOGO_URL" id="logo" alt="BLOG_TITLE">.
 # The URL may be relative to the site root.
-LOGO_URL = 'images/vozesdadiaspora_2.jpg'
-id = "logo"
-alt = "logo"
+LOGO_URL = 'images/vozesdadiaspora_2.jpg' id = "logo" alt = "logo"
 
 # If you want to hide the title of your website (for example, if your logo
 # already contains the text), set this to False.
